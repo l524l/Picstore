0e61aae1c872af7fe1c22b9baf4a69abf6e6c5c2
l524l <perschin998@gmail.com>
2022-02-12 10:55:32 +0300

    init dev

--- src/main/java/site/l524l/picstore/category/Category.java
+++ src/main/java/site/l524l/picstore/category/Category.java
@@ -6,4 +6,20 @@ import javax.persistence.Entity;
 public class Category {
 	private String name;
 	private boolean isNsfw;
+
+	public boolean isNsfw() {
+		return isNsfw;
+	}
+
+	public void setNsfw(boolean isNsfw) {
+		this.isNsfw = isNsfw;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
