11b91e8ad48dc2c5ecf6cd3ead152c37af117d7b
l524l <perschin998@gmail.com>
2022-03-30 17:05:41 +0300

    Добавлена работа во внешнем томкат сервере

--- src/main/java/site/l524l/picstore/PicstoreApplication.java
+++ src/main/java/site/l524l/picstore/PicstoreApplication.java
@@ -3,12 +3,13 @@ package site.l524l.picstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan
 @EnableAutoConfiguration
-public class PicstoreApplication {
+public class PicstoreApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(PicstoreApplication.class, args);
 	}
